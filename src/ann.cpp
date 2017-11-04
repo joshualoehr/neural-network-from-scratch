7873160d7dc0e40e09bc954c8d080c234bf66b2d
Josh Loehr <loehrj@wwu.edu>
2017-11-03 17:31:31 -0700

    Expanded on FeedForward and other things to allow for multilayer stuff, sort of

--- src/ann.cpp
+++ src/ann.cpp
@@ -1,6 +1,10 @@
 #include "proto.hpp"
 
-MatrixXf FeedForward(MatrixXf inputs, MatrixXf weights, VectorXf bias);
+MatrixXf BackProp(MatrixXf inputs, MatrixXf weights, VectorXf bias, float lr);
+float CalcError(MatrixXf outputs, MatrixXf target_outputs);
+MatrixXf FeedForward_Matrix(MatrixXf inputs, MatrixXf weights, VectorXf bias);
+VectorXf FeedForward_Vector(VectorXf inputs, MatrixXf weights, VectorXf bias);
+MatrixXf InitWeights(int rows, int cols, float max_weight);
 
 struct DataSets
 {
@@ -11,34 +15,122 @@ struct DataSets
     DataSet test_set;
 };
 
+string mdim(MatrixXf m)
+{
+    stringstream ss;
+    ss << m.rows() << "x" << m.cols();
+    return ss.str();
+}
+
+string vdim(VectorXf v)
+{
+    stringstream ss;
+    ss << v.rows() << "x" << v.cols();
+    return ss.str();
+}
+
 int main()
 {
+    // Reset the random generator seed
+    srand((unsigned int) time(0));
+
     // MatrixXf csv_contents = ParseInputCSV(INPUT_FILENAME_SMALL);
     // cout << csv_contents << endl;
     // cout << endl << endl;
 
-    MatrixXf m(2,1);
-    m(0,0) = 1;
-    m(1,0) = 0;
-    cout << "inputs: " << endl << m << endl << endl;
+    MatrixXf X(4,2);
+    X(0,0) = 0;
+    X(0,1) = 0;
+    X(1,0) = 0;
+    X(1,1) = 1;
+    X(2,0) = 1;
+    X(2,1) = 0;
+    X(3,0) = 1;
+    X(3,1) = 1;
+    cout << "inputs: " << endl << X << endl << endl;
 
-    MatrixXf weights(2,1);
-    weights(0,0) = 20;
-    weights(1,0) = 20;
-    cout << "weights: " << endl << weights << endl << endl;
+    int L_0 = X.cols();
 
-    VectorXf bias = VectorXf::Constant(m.cols(), -10);
-    cout << "bias: " << endl << bias << endl << endl;
+    MatrixXf Y(4,1);
+    Y(0,0) = 0;
+    Y(1,0) = 1;
+    Y(2,0) = 1;
+    Y(3,0) = 0;
+    cout << "target output: " << endl << Y << endl << endl;
 
-    MatrixXf ff1 = FeedForward(m, weights, bias);
-    cout << ff1.rows() << "x" << ff1.cols() << endl;
-    cout << ff1 << endl;
+    int L_1 = 2;
+    int L_2 = 1;
+
+    MatrixXf W_1 = InitWeights(L_0, L_1, 20);
+    cout << "W_1: " << endl << W_1 << endl << endl;
+    VectorXf b_1 = VectorXf::Ones(L_1);
+
+    MatrixXf Z_1 = FeedForward_Matrix(X, W_1, b_1);
+    cout << "Z_1: " << endl << Z_1 << endl << endl;
+
+    MatrixXf A_1 = Sigmoid(Z_1);
+    cout << "A_1: " << endl << A_1 << endl << endl;
+
+    MatrixXf W_2 = InitWeights(L_1, L_2, 20);
+    cout << "W_2: " << endl << W_2 << endl << endl;
+    VectorXf b_2 = VectorXf::Ones(L_2);
+
+    MatrixXf Z_2 = FeedForward_Matrix(A_1, W_2, b_2);
+    cout << "Z_2: " << endl << Z_2 << endl << endl;
+
+    MatrixXf A_2 = Sigmoid(Z_2);
+    cout << "A_2: " << endl << A_2 << endl << endl;
+
+    cout << "error: " << CalcError(A_2, Y) << endl;
+
     return 0;
 }
 
-MatrixXf FeedForward(MatrixXf inputs, MatrixXf weights, VectorXf bias)
+MatrixXf BackProp(MatrixXf inputs, MatrixXf weights, VectorXf bias, float lr)
 {
-    return Sigmoid(weights.transpose() * inputs + bias);
+
+
+    return weights;
+}
+
+float CalcError(MatrixXf outputs, MatrixXf target_outputs)
+{
+    assert(outputs.rows() == target_outputs.rows() && outputs.cols() == target_outputs.cols()
+            && "Produced output dimensions did not match target dimensions");
+
+    float output, t_output, error = 0.0;
+    for (int r = 0; r < outputs.rows(); r++)
+    {
+        output = outputs(r,0);
+        t_output = target_outputs(r,0);
+        cout << pow(t_output - output, 2) << endl;
+        error += pow(t_output - output, 2);
+    }
+
+    return error;
+}
+
+
+MatrixXf InitWeights(int rows, int cols, float max_weight)
+{
+    return MatrixXf::Random(rows, cols) * max_weight;
+}
+
+MatrixXf FeedForward_Matrix(MatrixXf inputs, MatrixXf weights, VectorXf bias)
+{
+    MatrixXf Z(inputs.rows(), weights.cols());
+    for (int r = 0; r < inputs.rows(); r++)
+    {
+        VectorXf x = inputs.row(r).transpose();
+        VectorXf z = FeedForward_Vector(x, weights, bias);
+        Z.row(r) = z.transpose();
+    }
+    return Z;
+}
+
+VectorXf FeedForward_Vector(VectorXf inputs, MatrixXf weights, VectorXf bias)
+{
+    return weights.transpose() * inputs + bias;
 }
 
 MatrixXf Sigmoid(MatrixXf X)
